fc95ce0e1b231b007bf9f43972484a2d07c31e09
Satyam Tomar <143343967+SatyamStellar@users.noreply.github.com>
2024-03-21 22:09:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities ({
+    '.rotate-y-180': {
+      transform: "rotateY(180deg)"
+    },
+    '.-rotate-y-180': {
+      transform: "rotateY(-180deg)"
+    }
+  })
+})
+
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "color-primary": "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "#fff",
+        "color-blob": "#A427DF",
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '20px',
+        md: "50px"
+      }
+    }
+  },
+  plugins: [rotateY],
+}
